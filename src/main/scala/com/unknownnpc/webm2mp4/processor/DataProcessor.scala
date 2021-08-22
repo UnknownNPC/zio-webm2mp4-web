3ef6cd231518148f9b8a8ea4a50af0fd0227224f
Vitalii Zymukha <unknownvzzv@gmail.com>
2021-08-22 21:55:07 +0300

    Fix interface return type

--- src/main/scala/com/unknownnpc/webm2mp4/processor/DataProcessor.scala
+++ src/main/scala/com/unknownnpc/webm2mp4/processor/DataProcessor.scala
@@ -3,7 +3,7 @@ package com.unknownnpc.webm2mp4.processor
 import com.unknownnpc.webm2mp4.converter.Chunk2FileConverter.Chunk2FileConverterService
 import com.unknownnpc.webm2mp4.converter.Webm2Mp4Converter.Webm2Mp4ConverterService
 import com.unknownnpc.webm2mp4.validator.InputValidator.InputValidatorService
-import zio.{Chunk, Has, URLayer, ZIO, ZLayer}
+import zio.{Chunk, Has, IO, URLayer, ZLayer}
 
 import java.io.File
 
@@ -16,13 +16,16 @@ object DataProcessor {
   type DataProcessorService = Has[DataProcessor.Service]
 
   trait Service {
-    def process(data: Chunk[Byte]): ZIO[Chunk2FileConverterService with
-      Webm2Mp4ConverterService with InputValidatorService, DataProcessorError, File]
+    def process(data: Chunk[Byte]): IO[DataProcessorError, File]
   }
 
   val live: URLayer[Chunk2FileConverterService with
     Webm2Mp4ConverterService with InputValidatorService, DataProcessorService] = ZLayer.fromServices {
-    (a, b, c) =>
+    (a, b, c) => new Service {
+      override def process(data: Chunk[Byte]): IO[DataProcessorError, File] = {
+
+      }
+    }
   }
 
 }
